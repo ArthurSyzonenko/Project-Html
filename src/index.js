415ddd39b75dbd6141d719ac7976d0d95b73892d
ArthurSyzonenko <artursjaba@gmail.com>
2022-06-04 11:53:48 +0300

    MMy home work
    !

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import './sass/main.scss';
+import './animate.css';
+// import "./js/about-modal";
+import "./js/poduct-modal";
+import "./js/menu";
+import "./js/header-modal";
+
+
+
+import "./js/modal";
+import "./js/slider";
+
+// import "./js/modal";
+import "./js/mobile-menu";
+
+import "./js/wow.min";
+import "./js/contacts";
+
+
+
+
+
+
+
+
+
+
+
